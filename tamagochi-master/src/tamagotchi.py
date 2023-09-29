94f8a0dfcbc9945fb9373d41e8ce9d624987562f
Kurt <128549070+kurt-schleich@users.noreply.github.com>
2023-09-29 15:49:50 -0300

    Add files via upload

---
+++ tamagochi-master/src/tamagotchi.py
@@ -0,0 +1,46 @@
+class Tamagotchi:
+
+    def __init__(self, energiaMax:int, saciedadeMax:int, limpezaMax:int, idadeMax:int ):
+        pass
+
+    def getEnergiaMax(self):
+        return -1
+
+    def getSaciedadeMax(self):
+        return -1
+
+    def getLimpezaMax(self):
+        return -1
+
+    def getIdadeMax(self):
+        return -1
+
+    def getEnergiaAtual(self):
+        return -1
+
+    def getSaciedadeAtual(self):
+        return -1
+
+    def getLimpezaAtual(self):
+        return -1
+
+    def getIdadeAtual(self):
+        return -1
+
+    def getDiamantes(self):
+        return -1
+
+    def getEstaVivo(self):
+        return True
+
+    def brincar(self):
+        return True
+
+    def comer(self):
+        return True
+
+    def dormir(self):
+        return True
+
+    def banhar(self):
+        return True
